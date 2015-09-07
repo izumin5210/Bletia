eea73901a009215e6d96f442b3047da8d1f1556c
izumin5210 <masayuki@izumin.info>
2015-09-07 14:19:27 +0900

    Add BleStatus

---
+++ Bletia/src/main/java/info/izumin/android/bletia/BleStatus.java
@@ -0,0 +1,13 @@
+package info.izumin.android.bletia;
+
+/**
+ * Created by izumin on 9/7/15.
+ */
+public enum BleStatus {
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED,
+    SERVICE_DISCOVERING,
+    READY,
+    DISCONNECTING
+}
