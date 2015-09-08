e264862478b4f9a484c0998b834e46a198af0f3b
izumin5210 <masayuki@izumin.info>
2015-09-09 02:43:30 +0900

    Add BleEvent class

---
+++ Bletia/src/main/java/info/izumin/android/bletia/BleEvent.java
@@ -0,0 +1,33 @@
+package info.izumin.android.bletia;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+
+import org.jdeferred.Deferred;
+
+/**
+ * Created by izumin on 9/7/15.
+ */
+public class BleEvent {
+    enum Type {
+        WRITING_CHARACTERISTIC
+    }
+
+    private final Deferred mDeferred;
+    private BluetoothGattCharacteristic mCharacteristic;
+
+    public BleEvent(Deferred deferred) {
+        mDeferred = deferred;
+    }
+
+    public <D, F, P> Deferred<D, F, P> getDeferred() {
+        return mDeferred;
+    }
+
+    public BluetoothGattCharacteristic getCharacteristic() {
+        return mCharacteristic;
+    }
+
+    public void setCharacteristic(BluetoothGattCharacteristic characteristic) {
+        mCharacteristic = characteristic;
+    }
+}
