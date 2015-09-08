30e71fcad4579879ae4029fdf3c88283848b0388
izumin5210 <masayuki@izumin.info>
2015-09-09 02:43:22 +0900

    Implement test for Bletia#writeCharacteristic()

---
+++ Bletia/src/androidTest/java/info/izumin/android/bletia/BletiaTest.java
@@ -0,0 +1,75 @@
+package info.izumin.android.bletia;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+
+import org.jdeferred.DoneCallback;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.reflection.Whitebox;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import info.izumin.android.bletia.wrapper.BluetoothGattWrapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by izumin on 9/8/15.
+ */
+@RunWith(AndroidJUnit4.class)
+public class BletiaTest extends AndroidTestCase {
+
+    @Mock private BluetoothGattCharacteristic mCharacteristic;
+    @Mock private BluetoothGattWrapper mBluetoothGattWrapper;
+
+    private BluetoothGattCallbackHandler mCallbackHandler;
+    private Bletia mBletia;
+    private Context mContext;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mContext = getContext();
+        mBletia = new Bletia(mContext);
+        mCallbackHandler = new BluetoothGattCallbackHandler();
+        Whitebox.setInternalState(mBletia, "mCallbackHandler", mCallbackHandler);
+    }
+
+    @Test
+    public void writeCharacteristicSuccessfully() throws Exception {
+        Whitebox.setInternalState(mBletia, "mGattWrapper", mBluetoothGattWrapper);
+        final CountDownLatch latch = new CountDownLatch(1);
+        when(mBluetoothGattWrapper.writeCharacteristic(mCharacteristic)).thenReturn(true);
+        when(mCharacteristic.getUuid()).thenReturn(UUID.randomUUID());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mBletia.writeCharacteristic(mCharacteristic).then(new DoneCallback<BluetoothGattCharacteristic>() {
+                    @Override
+                    public void onDone(BluetoothGattCharacteristic result) {
+                        latch.countDown();
+                    }
+                });
+            }
+        }).start();
+
+        Thread.sleep(1000);
+        mCallbackHandler.onCharacteristicWrite(
+                mBluetoothGattWrapper, mCharacteristic, BluetoothGatt.GATT_SUCCESS);
+
+        boolean res = latch.await(1000, TimeUnit.MILLISECONDS);
+
+        assertThat(res).isTrue();
+    }
+}
