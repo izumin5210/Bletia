c71c7505e222ddcb2d56868c7992e2fca8f5e42a
izumin5210 <masayuki@izumin.info>
2015-09-10 21:13:23 +0900

    Add BletiaListener and EventEmitter

---
+++ Bletia/src/main/java/info/izumin/android/bletia/BletiaListener.java
@@ -0,0 +1,10 @@
+package info.izumin.android.bletia;
+
+/**
+ * Created by izumin on 9/10/15.
+ */
+public interface BletiaListener {
+    void onConnect();
+    void onDisconnect();
+    void onError(BleStatus status);
+}
---
+++ Bletia/src/main/java/info/izumin/android/bletia/EventEmitter.java
@@ -0,0 +1,38 @@
+package info.izumin.android.bletia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by izumin on 9/10/15.
+ */
+public class EventEmitter {
+
+    private List<BletiaListener> mListeners = new ArrayList<>();
+
+    public void addListener(BletiaListener listener) {
+        mListeners.add(listener);
+    }
+
+    public void removeListener(BletiaListener listener) {
+        mListeners.remove(listener);
+    }
+
+    public void emitConnectEvent() {
+        for (BletiaListener listener : mListeners) {
+            listener.onConnect();
+        }
+    }
+
+    public void emitDisconnectEvent() {
+        for (BletiaListener listener : mListeners) {
+            listener.onDisconnect();
+        }
+    }
+
+    public void emitError(BleStatus status) {
+        for (BletiaListener listener : mListeners) {
+            listener.onError(status);
+        }
+    }
+}
