02a00403034e39c97ec9a55dc888bfa188453fff
izumin5210 <masayuki@izumin.info>
2015-09-14 20:14:47 +0900

    Add BleEvent#obtainMessage()

--- Bletia/src/main/java/info/izumin/android/bletia/BleEvent.java
+++ Bletia/src/main/java/info/izumin/android/bletia/BleEvent.java
@@ -1,6 +1,8 @@
 package info.izumin.android.bletia;
 
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Bundle;
+import android.os.Message;
 
 import org.jdeferred.Deferred;
 
@@ -11,9 +13,23 @@ import java.util.UUID;
  */
 public class BleEvent {
     enum Type {
-        WRITING_CHARACTERISTIC,
-        READING_CHARACTERISTIC
+        WRITE_CHARACTERISTIC(1),
+        READ_CHARACTERISTIC(2),
+        WRITE_DESCRIPTOR(3),
+        READ_DESCRIPTOR(4);
+
+        private final int mWhat;
+
+        Type(int what) {
+            mWhat = what;
+        }
+
+        public int getWhat() {
+            return mWhat;
+        }
     }
+
+    public static final String KEY_UUID = "event uuid";
 
     private final UUID mUuid;
     private final Deferred mDeferred;
@@ -38,5 +54,14 @@ public class BleEvent {
 
     public void setCharacteristic(BluetoothGattCharacteristic characteristic) {
         mCharacteristic = characteristic;
+    }
+
+    public Message obtainMessage() {
+        Message message = Message.obtain();
+        message.what = mType.getWhat();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(KEY_UUID, mUuid);
+        message.setData(bundle);
+        return message;
     }
 }
