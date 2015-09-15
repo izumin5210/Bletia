5cf8aa2c2f259c18c2e9cfea7c2b4999e9e9e376
izumin5210 <masayuki@izumin.info>
2015-09-15 17:40:01 +0900

    Add 2 new callback methods to BletiaListener

--- bletia/src/main/java/info/izumin/android/bletia/BletiaListener.java
+++ bletia/src/main/java/info/izumin/android/bletia/BletiaListener.java
@@ -1,10 +1,14 @@
 package info.izumin.android.bletia;
 
+import android.bluetooth.BluetoothGattCharacteristic;
+
 /**
  * Created by izumin on 9/10/15.
  */
 public interface BletiaListener {
-    void onConnect();
-    void onDisconnect();
+    void onConnect(Bletia bletia);
+    void onDisconnect(Bletia bletia);
     void onError(BletiaException exception);
+    void onServicesDiscovered(Bletia bletia, int status);
+    void onCharacteristicChanged(Bletia bletia, BluetoothGattCharacteristic characteristic, int status);
 }
