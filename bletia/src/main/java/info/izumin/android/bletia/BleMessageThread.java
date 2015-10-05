e98a552f8e7292068c0344a09201bec9903fd925
izumin5210 <masayuki@izumin.info>
2015-10-05 13:33:22 +0900

    Replace BleActionStore with ActionQueueContainer on BleMessageThread

--- bletia/src/main/java/info/izumin/android/bletia/BleMessageThread.java
+++ bletia/src/main/java/info/izumin/android/bletia/BleMessageThread.java
@@ -6,7 +6,15 @@ import android.os.Message;
 
 import org.jdeferred.Promise;
 
+import java.util.UUID;
+
 import info.izumin.android.bletia.action.Action;
+import info.izumin.android.bletia.action.EnableNotificationAction;
+import info.izumin.android.bletia.action.ReadCharacteristicAction;
+import info.izumin.android.bletia.action.ReadDescriptorAction;
+import info.izumin.android.bletia.action.ReadRemoteRssiAction;
+import info.izumin.android.bletia.action.WriteCharacteristicAction;
+import info.izumin.android.bletia.action.WriteDescriptorAction;
 import info.izumin.android.bletia.wrapper.BluetoothGattWrapper;
 
 /**
@@ -18,21 +26,21 @@ public class BleMessageThread extends Handler {
 
     private final HandlerThread mHandlerThread;
     private final BluetoothGattWrapper mGattWrapper;
-    private final BleActionStore mActionStore;
+    private final ActionQueueContainer mQueueContainer;
 
-    public BleMessageThread(HandlerThread handlerThread, BluetoothGattWrapper gattWrapper, BleActionStore actionStore) {
+    public BleMessageThread(HandlerThread handlerThread, BluetoothGattWrapper gattWrapper, ActionQueueContainer queueContainer) {
         super(handlerThread.getLooper());
         mHandlerThread = handlerThread;
         mGattWrapper = gattWrapper;
-        mActionStore = actionStore;
+        mQueueContainer = queueContainer;
     }
 
     public void stop() {
         mHandlerThread.quitSafely();
     }
 
-    public <T> Promise<T, BletiaException, Object> execute(Action<T> action) {
-        mActionStore.enqueue(action);
+    public <T> Promise<T, BletiaException, Object> execute(Action<T, ?> action) {
+        ActionMessageHandler.valueOf(action.getType()).enqueue(action, mQueueContainer);
         dispatchMessage(action.obtainMessage());
 
         return action.getDeferred().promise();
@@ -40,14 +48,10 @@ public class BleMessageThread extends Handler {
 
     @Override
     public void handleMessage(Message msg) {
-        Action.Type type = Action.Type.valueOf(msg.what);
-
-        if (mActionStore.isRunning(type)) {
+        if (!ActionMessageHandler.valueOf(msg.what).execute(msg, mQueueContainer, mGattWrapper)) {
             Message delayed = obtainMessage();
             delayed.copyFrom(msg);
             sendMessageDelayed(delayed, DELAY_MILLIS);
-        } else {
-            mActionStore.execute(type, mGattWrapper);
         }
     }
 }
