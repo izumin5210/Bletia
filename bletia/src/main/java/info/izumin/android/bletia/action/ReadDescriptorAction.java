16dafe8db01f2b7659890e9f5bf2d89353ec24c3
izumin5210 <masayuki@izumin.info>
2015-11-12 00:24:19 +0900

    Add DeferredStrategy and make actions extend new abstract action

--- bletia/src/main/java/info/izumin/android/bletia/action/ReadDescriptorAction.java
+++ bletia/src/main/java/info/izumin/android/bletia/action/ReadDescriptorAction.java
@@ -2,30 +2,15 @@ package info.izumin.android.bletia.action;
 
 import android.bluetooth.BluetoothGattDescriptor;
 
-import info.izumin.android.bletia.core.BleErrorType;
 import info.izumin.android.bletia.core.BletiaException;
-import info.izumin.android.bletia.core.wrapper.BluetoothGattWrapper;
+import info.izumin.android.bletia.core.action.AbstractReadDescriptorAction;
 
 /**
  * Created by izumin on 9/15/15.
  */
-public class ReadDescriptorAction extends DescriptorAction {
+public class ReadDescriptorAction extends AbstractReadDescriptorAction {
 
     public ReadDescriptorAction(BluetoothGattDescriptor descriptor) {
-        super(descriptor);
-    }
-
-    @Override
-    public Type getType() {
-        return Type.READ_DESCRIPTOR;
-    }
-
-    @Override
-    public boolean execute(BluetoothGattWrapper gattWrapper) {
-        if (!gattWrapper.readDescriptor(getDescriptor())) {
-            getDeferred().reject(new BletiaException(this, BleErrorType.OPERATION_INITIATED_FAILURE));
-            return false;
-        }
-        return true;
+        super(descriptor, new DeferredStrategy<BluetoothGattDescriptor, BletiaException>());
     }
 }
--- bletia/src/main/java/info/izumin/android/bletia/action/WriteCharacteristicAction.java
+++ bletia/src/main/java/info/izumin/android/bletia/action/WriteCharacteristicAction.java
@@ -2,30 +2,15 @@ package info.izumin.android.bletia.action;
 
 import android.bluetooth.BluetoothGattCharacteristic;
 
-import info.izumin.android.bletia.core.BleErrorType;
 import info.izumin.android.bletia.core.BletiaException;
-import info.izumin.android.bletia.core.wrapper.BluetoothGattWrapper;
+import info.izumin.android.bletia.core.action.AbstractWriteCharacteristicAction;
 
 /**
  * Created by izumin on 9/15/15.
  */
-public class WriteCharacteristicAction extends CharacteristicAction {
+public class WriteCharacteristicAction extends AbstractWriteCharacteristicAction {
 
     public WriteCharacteristicAction(BluetoothGattCharacteristic characteristic) {
-        super(characteristic);
-    }
-
-    @Override
-    public Type getType() {
-        return Type.WRITE_CHARACTERISTIC;
-    }
-
-    @Override
-    public boolean execute(BluetoothGattWrapper gattWrapper) {
-        if (!gattWrapper.writeCharacteristic(getCharacteristic())) {
-            getDeferred().reject(new BletiaException(this, BleErrorType.OPERATION_INITIATED_FAILURE));
-            return false;
-        }
-        return true;
+        super(characteristic, new DeferredStrategy<BluetoothGattCharacteristic, BletiaException>());
     }
 }
