83524deb752ddb6201655e3b8c3e733fd6ea8c38
izumin5210 <masayuki@izumin.info>
2015-11-13 23:08:33 +0900

    Rename ResolveStrategy -> ActionResolver

--- bletia-core/src/main/java/info/izumin/android/bletia/core/ActionResolver.java
+++ bletia-core/src/main/java/info/izumin/android/bletia/core/ActionResolver.java
@@ -3,7 +3,7 @@ package info.izumin.android.bletia.core;
 /**
  * Created by izumin on 11/11/15.
  */
-public interface ResolveStrategy<T, E extends Throwable> {
+public interface ActionResolver<T, E extends Throwable> {
     void resolve(T value);
     void reject(E throwable);
 }
